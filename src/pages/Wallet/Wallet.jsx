3746f3961305f35f0fd7044c81d2aab2146399ff
nezkazuneznau-spec <nezkazuneznau@gmaul.com>
2026-03-24 11:16:30 +0200

    fix Tasks.jsx

--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -3,7 +3,6 @@ import { getTransactions } from '../../api/index'
 import { useUserStore } from '../../store/userStore'
 import api from '../../api/index'
 import { TonConnectButton, useTonConnectUI, useTonWallet, toUserFriendlyAddress } from '@tonconnect/ui-react'
-import { Address } from '@ton/core'
 import './Wallet.css'
 
 const TX_ICONS = {
