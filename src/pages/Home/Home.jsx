5b7d70fd63875753c8ca3aa2d7e0690e92424666
nezkazuneznau-spec <nezkazuneznau@gmaul.com>
2026-04-05 14:27:00 +0300

    promo budget stats

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -178,12 +178,10 @@ export default function Home({ user, onTab, onCreate, onMyTasks, onSupport, onPa
           <div className="qa-lbl">ПАРТНЁРСТВО</div>
         </div>
         )}
-        {(minerStatus==='1' || minerStatus==='2') && (
         <div className="qa" onClick={onMiner}>
           <div className="qa-icon si-cyan">⛏</div>
           <div className="qa-lbl">МАЙНИНГ</div>
         </div>
-        )}
       </div>
     </div>
   )
